d7ca1460c7f3ff988868974ff2351d0915019ac9
lilloo04 <lilloo04@naver.com>
2026-03-25 17:53:05 +0900

    init: Spring Boot project setup

---
+++ src/main/java/com/quadcore/voiceandtext/VoiceandtextApplication.java
@@ -0,0 +1,13 @@
+package com.quadcore.voiceandtext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoiceandtextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoiceandtextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quadcore/voiceandtext/VoiceandtextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quadcore.voiceandtext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceandtextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
